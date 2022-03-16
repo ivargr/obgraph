372597c03f585512578f173e138ce57719693f50
Ivar Grytten <ivar.grytten@gmail.com>
2022-03-16 10:55:23 +0100

    Implemented new fast cython traversing of graph

--- obgraph/cython_traversing.pyx
+++ obgraph/cython_traversing.pyx
@@ -6,44 +6,65 @@ import time
 
 
 @cython.wraparound(False)
-cdef traverse_graph_by_following_nodes(graph, follow_nodes, only_add_nodes_in_follow_nodes=False):
-    assert type(follow_nodes) == set
+def traverse_graph_by_following_nodes(graph, np.uint8_t[:] follow_nodes):
+    logging.info("Traversing with cython")
+
+    #assert type(follow_nodes) == np.ndarray
 
     logging.info("N nodes in follow set: %d" % len(follow_nodes))
 
-    nodes_found = []
-    linear_ref_nodes = graph.linear_ref_nodes()
-    current_node = graph.get_first_node()
-    i = 0
-    while True:
-        if current_node in follow_nodes:
-            nodes_found.append(current_node)
+    cdef np.ndarray[np.uint32_t] nodes_found = np.zeros(len(graph.nodes), dtype=np.uint32)
+    cdef int node_index = 0
 
-        next_nodes = graph.get_edges(current_node)
-        if len(next_nodes) == 0:
-            break
+    cdef np.int64_t[:] edges = graph.edges
+    cdef np.uint8_t[:] node_to_n_edges = graph.node_to_n_edges
+    cdef np.uint32_t[:] node_to_edge_index = graph.node_to_edge_index
+    
+    cdef np.uint8_t[:] linear_nodes_index = graph.linear_ref_nodes_and_dummy_nodes_index
 
-        next_node = None
-        if len(next_nodes) == 1:
-            next_node = next_nodes[0]
-        else:
-            for potential_next in next_nodes:
-                if potential_next in follow_nodes or (next_node is None and potential_next in linear_ref_nodes):
-                    next_node = potential_next
+    cdef np.int64_t[:] next_nodes
+    cdef int edge_i = 0
+    cdef int next_node = -1
+    cdef int j
 
-            # IF did not find any nodes, choose an empty node if there is one
-            if next_node is None:
-                next_node = [n for n in next_nodes if graph.get_node_size(n) == 0]
-                assert len(next_node) == 1, "There are multiple empty nodes from node %d: %s" % (
-                current_node, next_nodes)
-                next_node = next_node[0]
 
-        assert next_node is not None, "Could not find any new nodes from %d. Edges are %s" % (
-        current_node, str(next_nodes))
-        current_node = next_node
-        i += 1
+    #linear_ref_nodes = graph.linear_ref_nodes()
+    cdef int current_node
 
-    if current_node in follow_nodes:
-        nodes_found.append(current_node)
+    #print(type(current_node))
 
-    return nodes_found
+    for current_node in graph.chromosome_start_nodes:
+
+        while True:
+            #if current_node in follow_nodes:
+                #nodes_found.append(current_node)
+            nodes_found[node_index] = current_node
+            node_index += 1
+
+            #next_nodes = graph.get_edges(current_node)
+
+            edge_i = node_to_edge_index[current_node]
+            next_nodes = edges[edge_i:edge_i+node_to_n_edges[current_node]]
+
+            if len(next_nodes) == 0:
+                break
+
+            next_node = -1
+            if len(next_nodes) == 1:
+                next_node = next_nodes[0]
+            else:
+                for j in range(next_nodes.shape[0]):
+                #for potential_next in next_nodes:
+                    if follow_nodes[next_nodes[j]] == 1 or (next_node == -1 and linear_nodes_index[next_nodes[j]] == 1):
+                        next_node = next_nodes[j]
+
+                assert next_node != -1, "Could not find next node from node %d" % current_node
+
+            current_node = next_node
+
+    #if current_node in follow_nodes:
+    #nodes_found.append(current_node)
+    nodes_found[node_index] = current_node
+    node_index += 1
+
+    return nodes_found[0:node_index]
