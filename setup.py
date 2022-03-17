5e71bef589e170e27fff3a7f4f9b030af06ed338
Ivar Grytten <ivar.grytten@gmail.com>
2022-03-17 14:36:24 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(name='obgraph',
-      version='0.0.8',
+      version='0.0.9',
       description='obgraph',
       url='http://github.com/ivargr/obgraph',
       author='Ivar Grytten',
@@ -11,7 +11,7 @@ setup(name='obgraph',
       license='MIT',
       packages=["obgraph"],
       zip_safe=False,
-      install_requires=['numpy==1.20.3', 'tqdm', 'pathos', 'graph_kmer_index', 'shared_memory_wrapper'],
+      install_requires=['numpy==1.20.3', 'tqdm', 'pathos', 'graph_kmer_index>=0.0.15', 'shared_memory_wrapper>=0.0.7'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ],
